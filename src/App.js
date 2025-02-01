283c0b30d024e5841037ecda011d146d849fc0e1
Madonna-Mansour <madonnamansour57@gmail.com>
2025-02-01 11:08:13 -0800

    last

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import './header/Hedar.jsx'
+import Hedar from './header/Hedar.jsx';
+import style from './Style.css';
+import Pink from './header/Pink.jsx';
+import Navv from './header/Navvv.jsx'
+import Slid1 from './sleds/SlidOne.jsx';
+import Slid2 from './sleds/SlidTow.jsx';
+import Slid3 from './sleds/SlidThree.jsx';
+import Slid4 from './sleds/SlidFour.jsx';
+import SlidFour1 from './sleds/SlidFour1.jsx';
+import SlidFive from './sleds/SlidFive.jsx';
+import SlidSix from './sleds/SlidSix.jsx';
+import SlidSeven from './sleds/SlidSeven.jsx';
+import SlideEat from './sleds/SlideEat.jsx';
+import SlidNighn from './sleds/SlidNighn.jsx';
+import SlidTen from './sleds/SlidTen.jsx';
+import SlidEliv from './sleds/SlidEliv.jsx';
+import Foot from './Footer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+   <Hedar/>
+   <Pink/>
+   <Navv/>
+   <Slid1/>
+   <Slid2/>
+   <Slid3/>
+   <SlidFour1/>
+   <Slid4/>
+   <SlidFive/>
+   <SlidSix/>
+   <SlidSeven/>
+   <SlideEat/>
+   <SlidNighn/>
+   <SlidTen/>
+   <SlidEliv/>
+   <Foot/>
+    </div>
+  );
+}
+
+export default App;
